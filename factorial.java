c489d37381d27fdc7fb22f632f7cee46497118ee
Hariharan2006K <hariharanmass682@gmail.com>
2025-08-27 11:47:03 +0530

    Factorial in Java

---
+++ factorial.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class factorial {
+    
+    public static int factorial(int n){
+        int f=1;
+
+        for(int i=1;i<=n;i++){
+            f=f*i;
+        }
+        return f;
+    }
+    
+    public static void main(String[] args) {
+        int a;
+        Scanner sc= new Scanner(System.in);
+        a=sc.nextInt();
+
+
+        System.out.println(factorial(a));
+    }
+}
