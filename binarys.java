1bd8037bbc8b947925267bf671118f93fcb0bce0
Hariharan2006K <hariharanmass682@gmail.com>
2025-09-02 23:58:43 +0530

    Binary Search in Java

---
+++ binarys.java
@@ -0,0 +1,16 @@
+public class binarys {
+    
+    public static int binarysearch(int[]arr,int target){
+        int start=0;
+        int end=arr.length-1;
+        while(start<=end){
+            
+        }
+
+    }
+    
+    public static void main(String[] args) {
+        int[]arr={1,2,3,4,5,6,7,8,9};
+        int target=5;
+    }
+}
