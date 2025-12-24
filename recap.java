9ebeb855eb040852e2bcdf2baa8984d67360f5e7
Hariharan2006K <hariharanmass682@gmail.com>
2025-12-24 13:20:30 +0530

    Recap of Java

---
+++ recap.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class recap {
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Hello world");
+        //int a=10;
+        //int b=sc.nextInt();
+        String name= sc.next();
+        System.out.println(name);
+        //System.out.println(a+b);
+    }
+}
